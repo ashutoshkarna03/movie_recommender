324f78a565ca283fdce449b13433646698fe1b66
ashutoshkarna03 <ashutosh@uxcam.com>
2020-04-17 02:06:56 +0545

    first version of movie recommender using Correlation

--- movie_recommender_using_correlation.py
+++ movie_recommender_using_correlation.py
@@ -45,3 +45,35 @@ plt.show()
 # joint plot of average ratings vs no of ratings
 sns.jointplot(x='rating', y='no_of_ratings', data=ratings, alpha=0.5)
 plt.show()
+
+# Now let's create a matrix that has the user ids on one access and the movie title on another axis
+movie_mat = user_data.pivot_table(index='user_id', columns='title', values='rating')
+pprint(movie_mat.head())
+
+# let's try recommendation for movie `Star Wars (1977)`
+
+# let's grab the user's rating for this movie
+starwars_user_ratings = movie_mat['Star Wars (1977)']
+pprint(starwars_user_ratings.head(10))
+# get the movies similar to starwars using co-relation
+similar_to_starwars = movie_mat.corrwith(starwars_user_ratings)
+
+corr_starwars = pd.DataFrame(similar_to_starwars, columns=['Correlation'])
+corr_starwars.dropna(inplace=True)
+pprint(corr_starwars.head())
+
+# let's check the result by sorting Correlation
+pprint(corr_starwars.sort_values('Correlation', ascending=False).head(10))
+
+# we find lots of movie having Correlation 1, but the number of ratings would be less than 100, so we need to filter out those also
+corr_starwars = corr_starwars.join(ratings['no_of_ratings'])
+pprint(corr_starwars.head())
+
+corr_starwars = corr_starwars[corr_starwars['no_of_ratings'] > 100]
+pprint(corr_starwars.sort_values('Correlation', ascending=False).head())
+recommendation_for_starwars = corr_starwars.sort_values('Correlation', ascending=False).head().index
+print('##############################')
+pprint(recommendation_for_starwars)
+print('##############################')
+
+# similarly recommendation for other movies can also be made
